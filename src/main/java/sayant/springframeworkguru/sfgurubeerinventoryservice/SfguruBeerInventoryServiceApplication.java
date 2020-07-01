cddd9a07a66e2307504910de18d8e23383fcd00e
Juanma Perales <sayantjm@gmail.com>
2020-07-01 19:46:08 +0200

    Initial commit. Closes #1

---
+++ src/main/java/sayant/springframeworkguru/sfgurubeerinventoryservice/SfguruBeerInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package sayant.springframeworkguru.sfgurubeerinventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfguruBeerInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfguruBeerInventoryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sayant/springframeworkguru/sfgurubeerinventoryservice/SfguruBeerInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sayant.springframeworkguru.sfgurubeerinventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfguruBeerInventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
