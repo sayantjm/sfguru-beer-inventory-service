2159ea57524ae13f93e45f20ec9ac37c081b7ac6
Juanma Perales <sayantjm@gmail.com>
2020-07-01 20:27:51 +0200

    Adding BeerInventoryDto and mapper. Closes #5

---
+++ src/main/java/sayant/springframeworkguru/sfgurubeerinventoryservice/mapper/BeerInventoryMapper.java
@@ -0,0 +1,16 @@
+package sayant.springframeworkguru.sfgurubeerinventoryservice.mapper;
+
+import org.mapstruct.Mapper;
+import sayant.springframeworkguru.sfgurubeerinventoryservice.domain.BeerInventory;
+import sayant.springframeworkguru.sfgurubeerinventoryservice.model.BeerInventoryDto;
+
+/**
+ * Created by sayantjm on 1/7/20
+ */
+@Mapper(uses = {DateMapper.class})
+public interface BeerInventoryMapper {
+
+    BeerInventory beerInventoryDtoToBeerInventory(BeerInventoryDto beerInventoryDTO);
+
+    BeerInventoryDto beerInventoryToBeerInventoryDto(BeerInventory beerInventory);
+}
---
+++ src/main/java/sayant/springframeworkguru/sfgurubeerinventoryservice/model/BeerInventoryDto.java
@@ -0,0 +1,25 @@
+package sayant.springframeworkguru.sfgurubeerinventoryservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+/**
+ * Created by sayantjm on 1/7/20
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerInventoryDto {
+    private UUID id;
+    private OffsetDateTime createdDate;
+    private OffsetDateTime lastModifiedDate;
+    private UUID beerId;
+    private String upc;
+    private Integer quantityOnHand;
+}
