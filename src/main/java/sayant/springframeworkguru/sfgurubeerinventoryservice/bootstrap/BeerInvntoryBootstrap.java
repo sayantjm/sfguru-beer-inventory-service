2fa4e17ea228cd4293a5dc98853aec11783ecbee
Juanma Perales <sayantjm@gmail.com>
2020-11-07 08:52:11 +0100

    Adding bootstrap class. Closes #8

---
+++ src/main/java/sayant/springframeworkguru/sfgurubeerinventoryservice/bootstrap/BeerInvntoryBootstrap.java
@@ -0,0 +1,59 @@
+package sayant.springframeworkguru.sfgurubeerinventoryservice.bootstrap;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import sayant.springframeworkguru.sfgurubeerinventoryservice.domain.BeerInventory;
+import sayant.springframeworkguru.sfgurubeerinventoryservice.repository.BeerInventoryRepository;
+
+import java.util.UUID;
+
+/**
+ * Created by sayantjm on 7/11/20
+ */
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class BeerInvntoryBootstrap implements CommandLineRunner {
+    public static final String BEER_1_UPC = "0631234200036";
+    public static final String BEER_2_UPC = "0631234300019";
+    public static final String BEER_3_UPC = "0083783375213";
+    public static final UUID BEER_1_UUID = UUID.fromString("0a818933-087d-47f2-ad83-2f986ed087eb");
+    public static final UUID BEER_2_UUID = UUID.fromString("a712d914-61ea-4623-8bd0-32c0f6545bfd");
+    public static final UUID BEER_3_UUID = UUID.fromString("026cc3c8-3a0c-4083-a05b-e908048c1b08");
+
+    private final BeerInventoryRepository beerInventoryRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        if(beerInventoryRepository.count() == 0){
+            loadInitialInv();
+        }
+    }
+
+    private void loadInitialInv() {
+        beerInventoryRepository.save(BeerInventory
+                .builder()
+                .beerId(BEER_1_UUID)
+                .upc(BEER_1_UPC)
+                .quantityOnHand(50)
+                .build());
+
+        beerInventoryRepository.save(BeerInventory
+                .builder()
+                .beerId(BEER_2_UUID)
+                .upc(BEER_2_UPC)
+                .quantityOnHand(50)
+                .build());
+
+        beerInventoryRepository.saveAndFlush(BeerInventory
+                .builder()
+                .beerId(BEER_3_UUID)
+                .upc(BEER_3_UPC)
+                .quantityOnHand(50)
+                .build());
+
+        log.debug("Loaded Inventory. Record count: " + beerInventoryRepository.count());
+    }
+}
