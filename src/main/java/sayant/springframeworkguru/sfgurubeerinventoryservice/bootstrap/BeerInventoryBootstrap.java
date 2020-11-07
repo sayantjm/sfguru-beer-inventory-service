3ba8249c3f1d7d44bf38829ce63e5641c406aefc
Juanma Perales <sayantjm@gmail.com>
2020-11-07 20:06:31 +0100

    Updating inventory quantities to be different

--- src/main/java/sayant/springframeworkguru/sfgurubeerinventoryservice/bootstrap/BeerInventoryBootstrap.java
+++ src/main/java/sayant/springframeworkguru/sfgurubeerinventoryservice/bootstrap/BeerInventoryBootstrap.java
@@ -44,14 +44,14 @@ public class BeerInventoryBootstrap implements CommandLineRunner {
                 .builder()
                 .beerId(BEER_2_UUID)
                 .upc(BEER_2_UPC)
-                .quantityOnHand(50)
+                .quantityOnHand(30)
                 .build());
 
         beerInventoryRepository.saveAndFlush(BeerInventory
                 .builder()
                 .beerId(BEER_3_UUID)
                 .upc(BEER_3_UPC)
-                .quantityOnHand(50)
+                .quantityOnHand(45)
                 .build());
 
         log.debug("Loaded Inventory. Record count: " + beerInventoryRepository.count());
