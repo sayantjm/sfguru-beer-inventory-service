856e904dec5c5412353ee422fe2d73660de5d7a5
Juanma Perales <sayantjm@gmail.com>
2021-01-22 17:18:03 +0100

    Adding JMS configuration. Closes #14

---
+++ src/main/java/sayant/springframeworkguru/sfgurubeerinventoryservice/config/JmsConfig.java
@@ -0,0 +1,21 @@
+package sayant.springframeworkguru.sfgurubeerinventoryservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+/**
+ * Created by sayantjm on 22/1/21
+ */
+@Configuration
+public class JmsConfig {
+    @Bean
+    public MessageConverter jacksonJmsMessageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+}
